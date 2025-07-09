c0dee070dade6706d3dfbe3f3df4fba057e8e834
532548651213215464 <qsima1044@gmail.com>
2025-07-09 16:33:30 +0800

    Create main.c

---
+++ ex1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("%s\n","Novemver 2024");
+    printf("%s\n","Mo Tu We Th Fr Sa Su");
+    printf("%*s\n",20,"1  2");
+    printf("%*s\n",20,"3  4  5  6  7  8  9");
+    printf("%*s\n",20,"10 11 12 13 14 15 16");
+    printf("%*s\n",20,"17 18 19 20 21 22 23");
+    printf("%*s\n",20,"24 25 26 27 28 29 30");
+    
+    return 0;
+}
