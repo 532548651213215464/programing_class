86f94f85d8c7c9be2ce89128bef5764920dd6ec0
532548651213215464 <qsima1044@gmail.com>
2025-08-13 10:36:47 +0800

    Create class6.c

---
+++ ex1/class6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    
+    
+    for (int num = 2;num <= 100;num++) {
+        
+        int isPrime = 1;
+        
+        for (int i = 2;i*i<=num;i++) {
+            
+            if (num%i == 0){
+              
+              isPrime = 0;
+              break;
+              
+            } else {
+                
+                
+            }
+            
+        }
+        
+        if (isPrime) {
+        
+            printf("%d",num);
+            printf("\n");
+        }
+       
+    }
+    
+     
+
+    return 0;
+    
+}
